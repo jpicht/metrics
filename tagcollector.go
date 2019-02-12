7e3a81190d82beda983758fa64d3926a35af9f1c
Julian Picht <julian.picht@gmail.com>
2019-02-12 12:03:52 +0100

    improve tagcollector

--- tagcollector.go
+++ tagcollector.go
@@ -10,7 +10,14 @@ type (
 	// TagCollector implements a convenience-interface to iteratively add tags
 	TagCollector struct {
 		sink Sink
-		tags map[string]string
+		tags *tag
+	}
+
+	// tag is the linked list element used inside TagCollector
+	tag struct {
+		name  string
+		value string
+		next  *tag
 	}
 )
 
@@ -18,46 +25,82 @@ type (
 func NewTagCollector(s Sink) *TagCollector {
 	return &TagCollector{
 		sink: s,
-		tags: map[string]string{},
+		tags: nil,
 	}
 }
 
 // Publish pushes the tags plus the given info via the selected sink implementation
 func (t *TagCollector) Publish(measurement string, values ...types.Field) {
-	t.sink.Publish(measurement, t.tags, values...)
+	t.sink.Publish(measurement, t.collect(), values...)
 }
 
 // PublishWithTimestamp pushes the tags plus the given info via the selected sink implementation
 func (t *TagCollector) PublishWithTimestamp(ts time.Time, measurement string, values ...types.Field) {
-	t.sink.PublishWithTimestamp(ts, measurement, t.tags, values...)
+	t.sink.PublishWithTimestamp(ts, measurement, t.collect(), values...)
 }
 
 // WithTag adds a tag to the TagCollector
 func (t *TagCollector) WithTag(name, value string) *TagCollector {
-	t.tags[name] = value
-	return t
+	return &TagCollector{
+		t.sink,
+		&tag{name, value, t.tags},
+	}
 }
 
 // WithTags adds a number of tags to the TagCollector
 func (t *TagCollector) WithTags(tags map[string]string) *TagCollector {
+	x := t
 	for name, value := range tags {
-		t.tags[name] = value
+		x = t.WithTag(name, value)
 	}
-	return t
+	return x
 }
 
 // WithTag creates a new TagCollector object with tag given and the default sink
 func WithTag(name, value string) *TagCollector {
 	return &TagCollector{
 		sink: DefaultSink,
-		tags: map[string]string{name: value},
+		tags: &tag{name, value, nil},
 	}
 }
 
 // WithTags creates a new TagCollector object with the given tags and the default sink
 func WithTags(tags map[string]string) *TagCollector {
-	return &TagCollector{
+	return emptyCollector.WithTags(tags)
+}
+
+var (
+	emptyCollector = &TagCollector{
 		sink: DefaultSink,
-		tags: tags,
+		tags: nil,
 	}
+)
+
+func (t *tag) count() int {
+	c := 0
+	e := t
+	for e != nil {
+		c++
+		e = e.next
+	}
+	return c
+}
+
+func (t *tag) collect() map[string]string {
+	result := make(map[string]string, t.count())
+	e := t
+	for e != nil {
+		if _, ok := result[e.name]; !ok {
+			result[e.name] = e.value
+		}
+		e = e.next
+	}
+	return result
+}
+
+func (t *TagCollector) collect() map[string]string {
+	if t.tags == nil {
+		return map[string]string{}
+	}
+	return t.tags.collect()
 }
